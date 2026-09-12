1e0d8fd203d0fc3be1659256e787be5a1ebba785
Gonzathegon <axelmoai@gmail.com>
2026-09-12 18:11:07 -0300

    consumicion de datos de productos de la base de datos y mostrados en un messagebox

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace sistema_gestion_heladeria.Models
 {
    public class Producto
     {
-        public int Id { get; set; }
+
+
+        [JsonProperty("id_producto")]
+        public int IdProducto { get; set; }
 
         public string Nombre { get; set; }
 
--- Views/PantallaPrincipal.xaml.cs
+++ Views/PantallaPrincipal.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using sistema_gestion_heladeria.Models;
 using sistema_gestion_heladeria.Controls;
+using sistema_gestion_heladeria.Services;
+
 
 namespace sistema_gestion_heladeria.Views
 {
@@ -34,13 +36,46 @@ namespace sistema_gestion_heladeria.Views
         }
 
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void Window_Loaded (object sender, RoutedEventArgs e)
         {
             CargarDatosUsuario();
 
+           
             //Segun el rol del usuario que userControl le renderiza
             CargarPanelSegunEstado();
+            
+             await ConsumirTodosProductos();
         }
+
+        private async Task ConsumirTodosProductos()
+{
+    ProductosService productosService = new ProductosService();
+
+    List<Producto> productos = await productosService.ConsumirTodosProductos();
+
+    string mensaje = "";
+
+    foreach (Producto producto in productos)
+    {
+        mensaje +=
+            $"ID: {producto.IdProducto}\n" +
+            $"Nombre: {producto.Nombre}\n" +
+            $"Descripción: {producto.Descripcion}\n" +
+            $"Precio: {producto.Precio}\n" +
+            $"Stock: {producto.Stock}\n" +
+            $"Categoría: {producto.Categoria}\n" +
+            $"Activo: {producto.Activo}\n" +
+            $"-------------------------\n";
+    }
+
+    if (productos.Count == 0)
+    {
+        MessageBox.Show("No se encontraron productos.");
+        return;
+    }
+
+    MessageBox.Show(mensaje, "Productos recibidos");
+}
 
 
         private void CargarPanelSegunEstado()
